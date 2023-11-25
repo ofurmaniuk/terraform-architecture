79c9584f8f46e080288d98e24044fde6a8de73f0
ofurmaniuk <142641592+ofurmaniuk@users.noreply.github.com>
2023-11-25 15:34:15 -0600

    added db instance

--- rds/main.tf
+++ rds/main.tf
@@ -16,3 +16,17 @@ resource "aws_db_subnet_group" "default" {
   name       = "main"
   subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
+
+# Creates db instance 
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "wordpress"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet_group_name  = aws_db_subnet_group.default.name
+}
