6fb43340773c59fd246ba31f23aa39a1e90fb3f0
ofurmaniuk <142641592+ofurmaniuk@users.noreply.github.com>
2023-11-25 15:26:54 -0600

    added main

---
+++ rds/main.tf
@@ -0,0 +1,20 @@
+# pulls information from another workspace
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "ofurmaniuk"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+
+# Creates subnet group with private subnets
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [
+    data.terraform_remote_state.vpc.outputs.private_subnets
+  ]
+}
